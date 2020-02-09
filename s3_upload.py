511e3478f0ee15db7559ea0898087b32e6ed3663
vinodcloud <60413861+vinodcloud@users.noreply.github.com>
2020-02-09 12:54:49 -0500

    Delete s3_upload.py

--- s3_upload.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import boto3
-s3_client = boto3.client('s3')
-response = s3_client.upload_file('/var/lib/jenkins/file1', 'cf-templates-z36e7fpqfst3-us-east-2', 'file1-2-9')
-print(response)
