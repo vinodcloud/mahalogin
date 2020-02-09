3b9cb86877db3347f239d848b353c08f4f96f49a
vinodcloud <60413861+vinodcloud@users.noreply.github.com>
2020-02-09 11:18:44 -0500

    Update s3_upload.py

--- s3_upload.py
+++ s3_upload.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import boto3
 s3_client = boto3.client('s3')
 response = s3_client.upload_file('/var/lib/jenkins/file1', 'cf-templates-z36e7fpqfst3-us-east-2', 'file1-2-9')
