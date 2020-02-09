061d29dec471532239766c84b122c379b84c6a3f
vinodcloud <60413861+vinodcloud@users.noreply.github.com>
2020-02-09 09:45:04 -0500

    Add files via upload

---
+++ fe9-sample.py
@@ -0,0 +1,5 @@
+import boto3
+from botocore.exceptions import ClientError
+s3_client = boto3.client('s3')
+response = s3_client.upload_file('/var/lib/jenkins/file1', 'cf-templates-z36e7fpqfst3-us-east-2', 'file1-2-9')
+print(response)
